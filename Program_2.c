023b126eb298960640cc340c4aa51fb882d95c34
eshan.one <108228113+eshan-one@users.noreply.github.com>
2022-08-12 14:28:24 +0530

    Create Program_2.c

---
+++ Program_2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void PRINT_N(int);
+int main()
+{
+  int N;
+  printf("Enter any number:\n");
+  scanf("%d",&N);
+  PRINT_N(N);
+  return 0;
+}
+
+void PRINT_N(int n)
+{
+  
+  
+  
+  if(n>0)
+  {
+    printf("\n%d",n);
+    printf("\n");
+    PRINT_N((n-1));
+    
+    
+
+  }
+  
+}
