5c12ec6519eca79954e611dc381f69494ae51b97
eshan.one <108228113+eshan-one@users.noreply.github.com>
2022-08-12 16:03:01 +0530

    Create Program_7.c

---
+++ Program_7.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void PRINT_N(int);
+int f1(int);
+int main()
+{
+  int N;
+  printf("Enter any number:\n");
+  scanf("%d",&N);
+  PRINT_N(N);
+  
+  return 0;
+}
+
+void PRINT_N(int n)
+{
+  
+  if(n>0)
+  {
+    PRINT_N((n-1));
+    printf("\n%d",n*n);
+    printf("\n");
+  }
+    
+}
