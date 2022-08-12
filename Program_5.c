b9aa9a6a5a3cadf0b773c3fa69c4f58164c9fe4b
eshan.one <108228113+eshan-one@users.noreply.github.com>
2022-08-12 15:30:59 +0530

    Create Program_5.c

---
+++ Program_5.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void PRINT_N(int);
+int f1(int);
+int main()
+{
+  int N;
+  printf("Enter any number:\n");
+  scanf("%d",&N);
+  f1(N);
+  
+  return 0;
+}
+// Here we need to create one more function which will 2X the value of n which is entered by the user.
+int f1(x)
+{
+  
+  x*=2;
+
+  PRINT_N(x);
+}
+
+void PRINT_N(int n)
+{
+  
+  
+  
+
+  if(n>=2)
+  {
+   
+    PRINT_N((n-2));
+
+    printf("\n%d",n);
+
+    printf("\n");
+
+  }
+    
+
+  
+  
+}
